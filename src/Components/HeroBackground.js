046d34a8a8450fc0fc19ca9bc96cdca1ab1a7d53
David Esquerra <davidesquerra.de@gmail.com>
2022-05-24 17:48:52 -0700

    added backgroud color to html page and things are lookign good! :)

--- src/Components/HeroBackground.js
+++ src/Components/HeroBackground.js
@@ -2,7 +2,7 @@
 
 const HeroBackground = () => {
     return (
-        <div class = 'h-screen overflow-hidden flex justify-center items-center'>
+        <div class = 'bg-emerald-100 h-screen overflow-hidden flex p-4 justify-center items-center'>
             <video loop autoPlay muted class='flex justify-center items-center'>
                 <source src='./Videos/hero.mp4' type='video/mp4' />
             </video>
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -2,12 +2,18 @@
 
 const Nav = () => {
     return (
-        <div class='p-2 justify-around bg-transparent w-full m-auto absolute flex top-0'> 
-            <h1 class='text-3xl text-center text-white'>Nav</h1>
-            <h1 class='text-3xl text-center text-white'>Nav</h1>
-            <h1 class='text-3xl text-center text-white'>Nav</h1>
-            <h1 class='text-3xl text-center text-white'>Nav</h1>
-            <h1 class='text-3xl text-center text-white'>Nav</h1>
+        <div class='bg-emerald-100 w-screen p-4 flex items-center justify-around'>
+            <div class='text-4xl tracking-wider pl-10 w-3/5 flex justify-start '>
+                <h1>Pure</h1>
+            </div>
+            <div class='flex justify-end w-2/5 pr-10'>
+                <ul class='flex justify-around w-4/5'>
+                    <li class='text-lg cursor-pointer'>Explore</li>
+                    <li class='text-lg'>Explore</li>
+                    <li class='text-lg'>Explore</li>
+                    <li class='text-lg'>Explore</li>
+                </ul>
+            </div>
         </div>
     );
 }
